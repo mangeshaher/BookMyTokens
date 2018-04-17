222a7ef8ac03abb9035239256e3899b4a6985c65
mangeshaher <mangeshaherngp@gmail.com>
2018-04-17 18:17:35 +0530

    added files

---
+++ BookMyTokens/app/src/main/java/com/example/mangeshaher/bookmytokens/BookTokens.java
@@ -0,0 +1,58 @@
+package com.example.mangeshaher.bookmytokens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class BookTokens extends AppCompatActivity {
+
+    private DatabaseReference dr ;
+    private ListView lv;
+    private List<Row> menulist ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_tokens);
+        menulist = new ArrayList<>();
+        dr = FirebaseDatabase.getInstance().getReferenceFromUrl("https://bookmytokens-a94d3.firebaseio.com/Menu");
+        lv = findViewById(R.id.menulist);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        dr.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                menulist.clear();
+                for(DataSnapshot menushot : dataSnapshot.getChildren()){
+                    Row row = menushot.getValue(Row.class);
+                    menulist.add(row);
+
+                }
+
+                MenuList adapter = new MenuList(BookTokens.this,menulist);
+                lv.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
